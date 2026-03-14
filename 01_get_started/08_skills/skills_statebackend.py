9c418e4d3ca1648be9647da8d2bd818d82614bab
lxhcaicai <2778763221@qq.com>
2026-03-14 18:32:35 +0800

    get started: skills_statebackend.py

---
+++ 01_get_started/08_skills/skills_statebackend.py
@@ -0,0 +1,54 @@
+import os
+from urllib.request import urlopen
+from deepagents import create_deep_agent
+from deepagents.backends.utils import create_file_data
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+
+#==================================
+
+BASE_URL = os.getenv("OPENAI_BASE_URL")      # 例如 https://api.your-service.com/v1
+API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL_NAME = os.getenv("OPENAI_MODEL")
+
+llm = ChatOpenAI(
+    model=MODEL_NAME,           # 模型名，会透传给服务端
+    base_url=BASE_URL,          # 自定义 base_url
+    api_key=API_KEY,            # 你的 API key
+    temperature=0.7
+    # max_tokens=..., timeout=..., 其他参数也可以直接写
+)
+
+#==================================
+
+checkpointer = MemorySaver()
+
+skill_url = "https://raw.githubusercontent.com/langchain-ai/deepagents/refs/heads/main/libs/cli/examples/skills/langgraph-docs/SKILL.md"
+with urlopen(skill_url) as response:
+    skill_content = response.read().decode('utf-8')
+
+skills_files = {
+    "/skills/langgraph-docs/SKILL.md": create_file_data(skill_content)
+}
+
+agent = create_deep_agent(
+    model=llm,
+    skills=["/skills/"],
+    checkpointer=checkpointer,
+)
+
+result = agent.invoke(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": "What is langgraph?",
+            }
+        ],
+        # Seed the default StateBackend's in-state filesystem (virtual paths must start with "/").
+        "files": skills_files
+    },
+    config={"configurable": {"thread_id": "12345"}},
+)
+
+print("result: "+ result["messages"][-1].content)
