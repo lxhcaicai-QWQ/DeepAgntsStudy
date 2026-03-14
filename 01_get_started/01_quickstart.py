b98a2a960595fc5615bc87955f6baa9a6e47cfed
lxhcaicai <2778763221@qq.com>
2026-03-14 11:48:52 +0800

    get started: quick start

---
+++ 01_get_started/01_quickstart.py
@@ -0,0 +1,57 @@
+import os
+from typing import Literal
+
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+from deepagents import create_deep_agent
+
+BASE_URL = os.getenv("OPENAI_BASE_URL")      # 例如 https://api.your-service.com/v1
+API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL_NAME = os.getenv("OPENAI_MODEL")
+
+tavily_client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
+
+def internet_search(
+    query: str,
+    max_results: int = 5,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = False,
+):
+    """Run a web search"""
+    return tavily_client.search(
+        query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic,
+    )
+
+# System prompt to steer the agent to be an expert researcher
+research_instructions = """You are an expert researcher. Your job is to conduct thorough research and then write a polished report.
+
+You have access to an internet search tool as your primary means of gathering information.
+
+## `internet_search`
+
+Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+"""
+
+llm = ChatOpenAI(
+    model=MODEL_NAME,           # 模型名，会透传给服务端
+    base_url=BASE_URL,          # 自定义 base_url
+    api_key=API_KEY,            # 你的 API key
+    temperature=0.7
+    # max_tokens=..., timeout=..., 其他参数也可以直接写
+)
+
+agent = create_deep_agent(
+    model=llm,
+
+    tools=[internet_search],
+    system_prompt=research_instructions,
+)
+
+result = agent.invoke({"messages": [{"role": "user", "content": "What is langgraph?"}]})
+
+# Print the agent's response
+print(result["messages"][-1].content)
+
