20b37b9cff9748aff95ade85a23ba783705824a9
lxhcaicai <2778763221@qq.com>
2026-03-14 14:01:35 +0800

    get started: filesystem_backend

---
+++ 01_get_started/05_backend/filesystem_backend.py
@@ -0,0 +1,28 @@
+import os
+
+from deepagents import create_deep_agent
+from deepagents.backends import FilesystemBackend
+from langchain_openai import ChatOpenAI
+
+BASE_URL = os.getenv("OPENAI_BASE_URL")      # 例如 https://api.your-service.com/v1
+API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL_NAME = os.getenv("OPENAI_MODEL")
+
+llm = ChatOpenAI(
+    model=MODEL_NAME,           # 模型名，会透传给服务端
+    base_url=BASE_URL,          # 自定义 base_url
+    api_key=API_KEY,            # 你的 API key
+    temperature=0.7
+    # max_tokens=..., timeout=..., 其他参数也可以直接写
+)
+
+agent = create_deep_agent(
+    model=llm,
+    backend=FilesystemBackend(root_dir=".", virtual_mode=True)
+)
+
+result1 = agent.invoke({"messages": [{"role": "user", "content": "帮我创建个test.txt,记录我今天出深圳湾公园玩的计划"}]})
+result2 = agent.invoke({"messages": [{"role": "user", "content": "从test.txt获取的内容是什么"}]})
+
+print("result1: "+ result1["messages"][-1].content)
+print("result2: "+ result2["messages"][-1].content)
