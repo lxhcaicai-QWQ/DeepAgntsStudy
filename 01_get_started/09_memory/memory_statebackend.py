b0daa669edfd88ab7af31ee7f01445b1d3f414d1
lxhcaicai <2778763221@qq.com>
2026-03-14 19:09:55 +0800

    get started: memory_statebackend.py

---
+++ 01_get_started/09_memory/memory_statebackend.py
@@ -0,0 +1,51 @@
+import os
+from urllib.request import urlopen
+
+from deepagents import create_deep_agent
+from deepagents.backends.utils import create_file_data
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+
+
+#==================================
+
+BASE_URL = os.getenv("OPENAI_BASE_URL")      # 例如 https://api.your-service.com/v1
+API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL_NAME = os.getenv("OPENAI_MODEL")
+
+llm = ChatOpenAI(
+    model=MODEL_NAME,           # 模型名，会透传给服务端
+    base_url=BASE_URL,          # 自定义 base_url
+    api_key=API_KEY,            # 你的 API key
+    temperature=0.7
+    # max_tokens=..., timeout=..., 其他参数也可以直接写
+)
+
+#==================================
+
+with urlopen("https://raw.githubusercontent.com/langchain-ai/deepagents/refs/heads/main/examples/text-to-sql-agent/AGENTS.md") as response:
+    agents_md = response.read().decode("utf-8")
+checkpointer = MemorySaver()
+
+agent = create_deep_agent(
+    memory=[
+        "/AGENTS.md"
+    ],
+    checkpointer=checkpointer,
+)
+
+result = agent.invoke(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": "Please tell me what's in your memory files.",
+            }
+        ],
+        # Seed the default StateBackend's in-state filesystem (virtual paths must start with "/").
+        "files": {"/AGENTS.md": create_file_data(agents_md)},
+    },
+    config={"configurable": {"thread_id": "123456"}},
+)
+
+print("result: "+ result["messages"][-1].content)
